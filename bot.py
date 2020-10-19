3093df6a61b1ff8af300102012449526e2adee1b
Vitaly <38107774+VitalyBlack@users.noreply.github.com>
2020-10-19 20:33:06 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,24 +1,31 @@
 import telebot
 from config import TOKEN
 
-
 bot = telebot.TeleBot(TOKEN)
-
-question = ('Интересная пара была?')
-options = ['Да', 'Нет']
 welcome = ('Введите /poll, чтобы начать опрос')
+
+class Poll():
+    def __init__(self, question, options):
+        self.question = question
+        self.options = options
+
+def send_poll(chat_id, poll):
+    bot.send_poll(chat_id, poll.question, poll.options)
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
     bot.send_message(message.chat.id, welcome)
 
+@bot.message_handler(commands=['poll'])
+def send_opros(message):
+    poll = Poll(
+        'Интересная пара была?',
+         ['Да', 'Нет']
+    )
+    send_poll(message.chat.id, poll)
 
-class Poll(question, options):
-    @bot.message_handler (commands= ['poll'])
-    def send_poll(self, message):
-        bot.send_poll(message.chat.id, Poll.question, Poll.options)
 
-
+bot.polling()
 
 
 
